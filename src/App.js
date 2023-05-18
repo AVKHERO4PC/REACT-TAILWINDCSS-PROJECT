5407d594b5b018aa6958ae32d8f082dfa1412f96
Vasanth <avkhero4pc@gmail.com>
2023-05-18 12:17:44 +0530

    done soem changes

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import { BrowserRouter as Router ,Routes,Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import ForgotPassword from "./pages/ForgotPassword"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-   <>
-   <Router>
-    <Routes>
-      <Route path ="/" element={<Home />}  >
-      <Route path ="/profile" element={<Profile />}  >
-      <Route path ="/forgot-password" element={<ForgotPassword />}  >
-      <Route path ="/sign-in" element={<SignIn />}  >
-      <Route path ="/sign-up" element={<SignUp />}  >
-
-      </Route>
-      </Route>
-      </Route>
-      </Route>
-      </Route>
-      
-    
-      
-    
-    </Routes>
-   </Router>
-   </>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
+
+      
+    
+      
+    
+    
+  
 
 export default App;
